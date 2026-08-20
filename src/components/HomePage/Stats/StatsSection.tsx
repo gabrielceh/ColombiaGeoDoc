f286115dfb265cad7afc7f39d200e818282e174c
gabrielceh <gabrielcehu@gmail.com>
2026-08-20 10:12:39 -0500

    modificando StatsSection

--- src/components/HomePage/Stats/StatsSection.tsx
+++ src/components/HomePage/Stats/StatsSection.tsx
@@ -1,31 +1,39 @@
 import styles from './stats-section.module.css';
 
+
+
 export function StatsSection() {
   return (
     <section className={styles.stats}>
-      <div className='container'>
-        <div className={styles.statsGrid}>
-          <div>
-            <strong>32</strong>
-            <span>Departamentos</span>
-          </div>
-
-          <div>
-            <strong>1.100+</strong>
-            <span>Municipios y ciudades</span>
-          </div>
-
-          <div>
-            <strong>REST</strong>
-            <span>Arquitectura</span>
-          </div>
-
-          <div>
-            <strong>JSON</strong>
-            <span>Formato de respuesta</span>
-          </div>
+      <div className={styles.container}>
+        <div className={styles.grid}>
+          {stats.map((stat) => (
+            <div className={styles.item} key={stat.label}>
+              <strong>{stat.value}</strong>
+              <span>{stat.label}</span>
+            </div>
+          ))}
         </div>
       </div>
     </section>
   );
 }
+
+const stats = [
+  {
+    value: '32',
+    label: 'Departamentos',
+  },
+  {
+    value: '1.100+',
+    label: 'Municipios y ciudades',
+  },
+  {
+    value: 'REST',
+    label: 'Arquitectura',
+  },
+  {
+    value: 'JSON',
+    label: 'Formato',
+  },
+];
